bd5cd0d45c73a02bc23bdc9fcad2ffec3deb9ef8
Gary Ash <gary.ash@icloud.com>
2024-08-04 21:01:40 -0400

    Initial commit.

---
+++ xcode/Templates/File Templates/Source/SwiftUI Model View.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,16 @@
+___FILEHEADER___
+
+import SwiftUI
+
+struct ___FILEBASENAMEASIDENTIFIER___: View {
+    var body: some View {
+        VStack {
+        }
+    }
+}
+
+struct ___FILEBASENAMEASIDENTIFIER___Previews: PreviewProvider {
+    static var previews: some View {
+        ___FILEBASENAMEASIDENTIFIER___()
+    }
+}
