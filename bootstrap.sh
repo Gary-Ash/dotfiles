6c1d50a3073ebc3632d9f3c5f012629005a056e8
Gary Ash <gary.ash@icloud.com>
2025-06-03 20:38:26 -0400

    Changed BBEdit key bindings
    Bound a smart new line script to Ctrl+Option+Cmd+Return

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,7 @@
 # Boot strap a new Mac setup
 #
 # Author   :  Gary Ash <gary.ash@icloud.com>
-# Created  :  17-May-2025  9:57pm
+# Created  :   1-Jun-2025  7:49pm
 # Modified :
 #
 # Copyright © 2024 By Gary Ash All rights reserved.
