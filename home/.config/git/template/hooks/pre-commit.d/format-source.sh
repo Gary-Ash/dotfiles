959998b544ee309e9f48b34cd6ce55e28e40aa75
Gary Ash <gary.ash@icloud.com>
2025-10-27 17:04:10 -0400

    [FEATURE] Split out my configuration

--- home/.config/git/template/hooks/pre-commit.d/format-source.sh
+++ home/.config/git/template/hooks/pre-commit.d/format-source.sh
@@ -8,7 +8,7 @@ shopt -s extglob
 #
 # Author   :  Gary Ash <gary.ash@icloud.com>
 # Created  :   4-Aug-2025  4:27pm
-# Modified :  23-Aug-2025  3:38pm
+# Modified :  28-Oct-2025  3:41pm
 #
 # Copyright © 2025 By Gary Ash All rights reserved.
 #*****************************************************************************************
@@ -23,35 +23,35 @@ for commit in $(git diff --cached --name-only --diff-filter=ACM); do
 	for filename in "${commit[@]}"; do
 		if [[ $filename =~ $swift ]]; then
 			if which swiftformat >/dev/null; then
-				swiftformat --config "$HOME/.config/.swiftformat" "${filename}"
-				git update-index --really-refresh --again "${filename}"
+				swiftformat --config "$HOME/.config/.swiftformat" "${filename}" &> /dev/null
+				git update-index --really-refresh --again "${filename}" &> /dev/null
 			fi
 		elif [[ $filename =~ $objc ]]; then
 			if which uncrustify >/dev/null; then
-				uncrustify -l OC+ --replace --no-backup -c "$HOME/.config/.uncrustify" "${filename}"
-				git update-index --really-refresh --again "${filename}"
+				uncrustify -l OC+ --replace --no-backup -c "$HOME/.config/.uncrustify" "${filename}" &> /dev/null
+				git update-index --really-refresh --again "${filename}" &> /dev/null
 			fi
 		elif [[ $filename =~ $cfiles ]]; then
 			if which uncrustify >/dev/null; then
-				uncrustify --replace --no-backup -c "$HOME/.config/.uncrustify" "${filename}"
+				uncrustify --replace --no-backup -c "$HOME/.config/.uncrustify" "${filename}" &> /dev/null
 				git update-index --really-refresh --again "${filename}"
 			fi
 		else
 			read -r line <"${filename}"
 			if [[ $line =~ ^\#\!.*perl ]]; then
 				if which perltidyrc >/dev/null; then
-					perltidy -b -bext='/' --profile="$HOME/.config/.perltidyrc" "${filename}"
-					git update-index --really-refresh --again "${filename}"
+					perltidy -b -bext='/' --profile="$HOME/.config/.perltidyrc" "${filename}" &> /dev/null
+					git update-index --really-refresh --again "${filename}" &> /dev/null
 				fi
 			elif [[ $line =~ ^\#\!.*python ]]; then
 				if which black >/dev/null; then
-					black --config "$HOME/.config/black" --quiet "${filename}"
-					git update-index --really-refresh --again "${filename}"
+					black --config "$HOME/.config/black" --quiet "${filename}" &> /dev/null
+					git update-index --really-refresh --again "${filename}" &> /dev/null
 				fi
 			elif [[ $line =~ ^\#\!.*(bash|zsh|sh)$ ]]; then
 				if which shfmt >/dev/null; then
-					shfmt -ln bash -i 0 -s -ci -w "${filename}"
-					git update-index --really-refresh --again "${filename}"
+					shfmt -ln bash -i 0 -s -ci -w "${filename}" &> /dev/null
+					git update-index --really-refresh --again "${filename}" &> /dev/null
 				fi
 			fi
 		fi
